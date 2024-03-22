13d1d4ced7a549cb010355754d158eb75e1829d5
Sachin4219 <elzachin373@gmail.com>
2024-03-22 12:01:19 +0530

    demo

--- client/client.js
+++ client/client.js
@@ -1,17 +1,18 @@
-const publicVapidKey =
-  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
-
 // Check for service worker
 if ("serviceWorker" in navigator) {
-  send().catch(err => console.error(err));
+  send().catch((err) => console.error(err));
 }
+const publickey =
+  "BKRns56lTgiccLbI4tVnvoBrzAeKhbDcZzVSR1Kexd2yVZS3mal9_lPL6Ec8nsYL64acQHgsZbyuC5WZsiTZDic";
+
+const base_url = "http://localhost:5000/subscribe";
 
 // Register SW, Register Push, Send Push
 async function send() {
   // Register Service Worker
   console.log("Registering service worker...");
   const register = await navigator.serviceWorker.register("/worker.js", {
-    scope: "/"
+    scope: "/",
   });
   console.log("Service Worker Registered...");
 
@@ -19,24 +20,24 @@ async function send() {
   console.log("Registering Push...");
   const subscription = await register.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    applicationServerKey: urlBase64ToUint8Array(publickey),
   });
   console.log("Push Registered...");
 
   // Send Push Notification
   console.log("Sending Push...");
-  await fetch("/subscribe", {
+  await fetch(base_url, {
     method: "POST",
     body: JSON.stringify(subscription),
     headers: {
-      "content-type": "application/json"
-    }
+      "content-type": "application/json",
+    },
   });
   console.log("Push Sent...");
 }
 
 function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, "+")
     .replace(/_/g, "/");
--- index.js
+++ index.js
@@ -10,15 +10,11 @@ app.use(express.static(path.join(__dirname, "client")));
 
 app.use(bodyParser.json());
 
-const publicVapidKey =
-  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
-const privateVapidKey = "3KzvKasA2SoCxsp0iIG_o9B0Ozvl1XDwI63JRKNIWBM";
+const publickey =
+  "BKRns56lTgiccLbI4tVnvoBrzAeKhbDcZzVSR1Kexd2yVZS3mal9_lPL6Ec8nsYL64acQHgsZbyuC5WZsiTZDic";
+const privatekey = "qxepqnkZ5K5KCZQtjxk4IxJHMVc4NyoP-iyb3So-PhY";
 
-webpush.setVapidDetails(
-  "mailto:test@test.com",
-  publicVapidKey,
-  privateVapidKey
-);
+webpush.setVapidDetails("mailto:test@test.com", publickey, privatekey);
 
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
@@ -34,7 +30,7 @@ app.post("/subscribe", (req, res) => {
   // Pass object into sendNotification
   webpush
     .sendNotification(subscription, payload)
-    .catch(err => console.error(err));
+    .catch((err) => console.error(err));
 });
 
 const port = 5000;
