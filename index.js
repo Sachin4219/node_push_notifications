73635a67dcfc5651c75966abec044295917aa115
Sachin4219 <elzachin373@gmail.com>
2024-03-22 14:28:56 +0530

    s

--- index.js
+++ index.js
@@ -2,38 +2,40 @@ const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
-
+app.use(cors());
 app.use(bodyParser.json());
 
 const publickey =
   "BKRns56lTgiccLbI4tVnvoBrzAeKhbDcZzVSR1Kexd2yVZS3mal9_lPL6Ec8nsYL64acQHgsZbyuC5WZsiTZDic";
 const privatekey = "qxepqnkZ5K5KCZQtjxk4IxJHMVc4NyoP-iyb3So-PhY";
 
-webpush.setVapidDetails("mailto:test@test.com", publickey, privatekey);
+webpush.setVapidDetails("mailto:elzachin373@gmail.com", publickey, privatekey);
 
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
   // Get pushSubscription object
   const subscription = req.body;
-
+  console.log(subscription);
   // Send 201 - resource created
   res.status(201).json({});
 
   // Create payload
-  const payload = JSON.stringify({ title: "Push Test" });
+  const payload = JSON.stringify(JSON.stringify({ title: "Push Test" }));
 
   // Pass object into sendNotification
   webpush
     .sendNotification(subscription, payload)
-    .then(() => res.json({ message: "success" }))
+    .then((r) => {
+      console.log(r);
+    })
     .catch((err) => {
       console.error(err);
-      res.json({ message: "failure" });
     });
 });
 
